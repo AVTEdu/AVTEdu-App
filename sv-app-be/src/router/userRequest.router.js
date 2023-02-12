69d0afed9b52cee00b9ffb57d784e0f55a5220fa
Viet Anh <vietanh6jk@gmail.com>
2023-02-12 16:09:55 +0700

    add verifiry Acess token

--- sv-app-be/src/router/userRequest.router.js
+++ sv-app-be/src/router/userRequest.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userRequestController = require("../controller/userRequest.controller");
+const { verifyAccessToken } = require('../helpers/jwt.service');
 const router = express.Router();
 
 router
@@ -8,5 +9,17 @@ router
 router
   .route("/getMonSinhVienChuaHoc")
   .post(userRequestController.getMonHocSinhVienChuaHoc);
+router
+  .route("/getLopHocPhanByHocPhan")
+  .post(userRequestController.getLopHocPhanByHocPhan);
+router
+  .route("/getChiTietHocPhan")
+  .post(userRequestController.getChiTietHocPhan);  
+router
+  .route("/dangKiHocPhan")
+  .post(verifyAccessToken,userRequestController.DangKiHocPhan);  
+router
+  .route("/getThongTinSinhVien")
+  .get(verifyAccessToken,userRequestController.getThongTinSinhvien);    
 
 module.exports = router;
