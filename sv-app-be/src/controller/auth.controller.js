64dbf01f83f8d915e1299a24381f5b3275b0d350
Viet Anh <vietanh6jk@gmail.com>
2023-02-12 16:50:07 +0700

    add more api for DangKiHocPhan

--- sv-app-be/src/controller/auth.controller.js
+++ sv-app-be/src/controller/auth.controller.js
@@ -6,6 +6,7 @@ const {
     signAccessToken,
     signRefreshToken,
     verifyRefreshToken,
+    verifyAccessToken,
   } = require("../helpers/jwt.service");
 const { createUserWithEmailAndPassword, 
   signInWithEmailAndPassword, 
@@ -36,24 +37,13 @@ const signIn = async (req,res,next) =>{
             .json({ error: { message: "Tài khoản hoặc mật khẩu không khớp !!!" } });
         }
         console.log("-----------------------Đã đăng nhập--------------------------");
-        //Đăng nhập vào firebase
-        signInWithEmailAndPassword(defaultAuth, sinh_vien.email, sinh_vien.ma_sinh_vien)
-        .then((userCredential) => {
-          // Signed in    
-          console.log("Đã đăng nhập "+sinh_vien.email);
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          console.log(errorCode);
-          console.log(errorMessage);
-        });
+       
         //Tạo accessToken
         const accessToken = await signAccessToken(ma);
         //Tạo refeshToken
         const refreshToken = await signRefreshToken(ma);
-        res.setHeader("authorization", accessToken);
-        res.setHeader("refreshToken", refreshToken);
+        await res.cookie('authorization', accessToken, { maxAge: 900000, httpOnly: true});
+        await res.cookie('refreshToken', refreshToken, { maxAge: 1900000, httpOnly: true});
         return res
         .status(200)
         .json({ success: true,accessToken,refreshToken, sinh_vien });
@@ -85,8 +75,8 @@ const signInAdmin = async (req,res,next) =>{
         const accessToken = await signAccessToken(ma);
         //Tạo refeshToken
         const refreshToken = await signRefreshToken(ma);
-        res.setHeader("authorization", accessToken);
-        res.setHeader("refreshToken", refreshToken);
+        await res.cookie('authorization', accessToken, { maxAge: 900000, httpOnly: true });
+        await res.cookie('refreshToken', refreshToken, { maxAge: 1900000, httpOnly: true });
         return res
         .status(200)
         .json({ success: true,accessToken,refreshToken, admin });
@@ -134,32 +124,47 @@ const AuthercationEmail= async (req, res, next) => {
 };
 const CheckVerificationEmail = async (req, res, next) => {
   try {
-    onAuthStateChanged(defaultAuth, (user) => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/firebase.User
-        const uid = user.uid;
-        console.log('id la:'+uid)
-        const emailVerified = user.emailVerified;
-        console.log(emailVerified);
-        if(!emailVerified)
-        sendEmailVerification(user)
-        .then(() => {
-          console.log ('Email verification sent!');
-        // ...
-        });
-         // ...
-      } else {
-        console.log('không có người dùng đăng nhập');
-      }
-    });
+    const userId = req.payload.userId;
+    const sinh_vien = await SinhVien.findOne({ where: { ma_sinh_vien:`${userId}` } });
+     //Đăng nhập vào firebase
+    await signInWithEmailAndPassword(defaultAuth, sinh_vien.email, sinh_vien.ma_sinh_vien)
+     .then((userCredential) => {
+       // Signed in    
+       console.log("Đã đăng nhập "+sinh_vien.email);
+       onAuthStateChanged(defaultAuth, (user) => {
+        if (user) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/firebase.User
+          const uid = user.uid;
+          console.log('id la:'+uid)
+          const emailVerified = user.emailVerified;
+          console.log(emailVerified);
+          if(!emailVerified){
+          sendEmailVerification(user)
+          .then(() => {
+            return res.status(200).json({ success:true,msg:"Email verification sent!"});
+          // ...
+          });
+           // ...
+        }else{
+          return res.status(200).json({ success:true,msg:"Email đã xác thực"+sinh_vien.email});
+        }} 
+      });
+     })
+     .catch((error) => {
+       const errorCode = error.code;
+       const errorMessage = error.message;
+       console.log(errorCode);
+       console.log(errorMessage);
+     });
+    
   } catch (error) {
     next(error);
   }
 };
 const Logout = async (req, res, next) => {
   try {
-    const { refreshToken } = req.body;
+    const { refreshToken } = req.cookies;
     // console.log(req.headers);
     if (!refreshToken) {
       return res.status(403).json({ message: "không có refreshtoken" });
@@ -169,6 +174,8 @@ const Logout = async (req, res, next) => {
     console.log(user)
     client.del(userId.toString(), (err, reply) => {
       if (err) return res.status(500).json({ message: "Lỗi không xác định" });
+      res.clearCookie("authorization");
+      res.clearCookie("refreshToken");
       return res.status(200).json({ message: "Đăng xuất thành công "+userId });
     });
   } catch (error) {
