3c3af456f7385eec7a00b5c36c37be8d7c3525ed
schoolgit15102001 <phamnvtruong15102001@gmail.com>
2023-02-20 22:57:17 +0700

    ui sidebaradmin

---
+++ sv-app-fe/src/component/admin/pages/QuanLyHocPhan.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+
+
+
+export const MonHoc = () => {
+    return (
+        <>
+            <Sidebar />
+            <div className="qlhp">
+                <h1>MonHoc page</h1>
+            </div>
+        </>
+    );
+};
+
+export const HocPhan = () => {
+    return (
+        <>
+            <Sidebar />
+            <div className="qlhp">
+                <h1>HocPhan page</h1>
+            </div>
+        </>
+    );
+};
---
+++ sv-app-fe/src/component/admin/pages/QuanLyPhong.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+
+
+export const LoaiPhong = () => {
+    return (
+        <>
+            <Sidebar />
+            <div className="qlph">
+                <h1>LoaiPhong page</h1>
+            </div>
+        </>
+    );
+};
+
+export const Phong = () => {
+    return (
+        <>
+            <Sidebar />
+            <div className="qlph">
+                <h1>Phong page</h1>
+            </div>
+        </>
+    );
+};
+
+
