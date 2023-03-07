426a4960c4402d04d7624298d89685ec4b0fed9a
Viet Anh <vietanh6jk@gmail.com>
2023-03-07 16:52:03 +0700

    fix api xacNhanThanhToan

--- sv-app-mobile/src/components/BottomTabNavigaton.jsx
+++ sv-app-mobile/src/components/BottomTabNavigaton.jsx
@@ -1,26 +1,88 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import  Icon  from 'react-native-vector-icons/MaterialIcons';
-import { View } from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import COLORS from '../consts/color';
+import {View} from 'react-native';
+import HomeScreen from '../screens/HomeScreen';
 
-const Tab= createBottomTabNavigator();
 
-const BottomTabNavigatior = () =>{
-    return(
-        <Tab.Navigator
-        screenOptions={{
-            style: {
-              height: 55,
-              borderTopWidth: 0,
-              elevation: 0,
-            },
-            showLabel: false,
-            activeTintColor: COLORS.primary,
-            headerShown:false
-          }}>
-            
+const Tab = createBottomTabNavigator();
 
-        </Tab.Navigator>
-    )
-}
+const BottomNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        style: {
+          height: 55,
+          borderTopWidth: 0,
+          elevation: 0,
+        },
+        showLabel: false,
+        activeTintColor: COLORS.primary,
+        headerShown:false
+      }}>
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Icon name="home-filled" color={color} size={28} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="LocalMall"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Icon name="local-mall" color={color} size={28} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <View
+              style={{
+                height: 60,
+                width: 60,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: COLORS.white,
+                borderColor: COLORS.primary,
+                borderWidth: 2,
+                borderRadius: 30,
+                top: -25,
+                elevation: 5,
+              }}>
+              <Icon name="search" color={COLORS.primary} size={28} />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Favorite"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Icon name="favorite" color={color} size={28} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Tài khoản"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Icon name="shopping-cart" color={color} size={28} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomNavigator;
