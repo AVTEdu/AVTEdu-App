5a60f49ca6546cdcd371893cbdbd2ffb3ff45b3b
Viet Anh <vietanh6jk@gmail.com>
2023-03-14 17:27:16 +0700

    add new screen Andrioid

---
+++ sv-app-mobile/src/sample/Date.js
@@ -0,0 +1,47 @@
+const DateTKB = [
+    {
+        ngay:"20/02/2023",
+        thu:"Thứ 2",
+        TKB:[
+            {
+                tiet:"1-3",
+                tenlop:"DHKTPM15B",
+                tenmon:"Lập trình WWW",
+                nhomthuchanh:0,
+                phong:"A3.02",
+                giangvien:"Nguyễn Văn A"
+            },
+            {
+                tiet:"1-3",
+                tenlop:"DHKTPM15B",
+                tenmon:"Lập trình WWW",
+                nhomthuchanh:0,
+                phong:"A3.02",
+                giangvien:"Nguyễn Văn A"
+            }
+        ]
+    },
+    {
+        ngay:"20/02/2023",
+        thu:"Thứ 2",
+        TKB:[
+            {
+                tiet:"1-3",
+                tenlop:"DHKTPM15B",
+                tenmon:"Lập trình WWW",
+                nhomthuchanh:0,
+                phong:"A3.02",
+                giangvien:"Nguyễn Văn A"
+            },
+            {
+                tiet:"1-3",
+                tenlop:"DHKTPM15B",
+                tenmon:"Lập trình WWW",
+                nhomthuchanh:0,
+                phong:"A3.02",
+                giangvien:"Nguyễn Văn A"
+            }
+        ]
+    }
+]
+export default DateTKB;
---
+++ sv-app-mobile/src/screens/DateScreen.jsx
@@ -0,0 +1,114 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import COLORS from "../consts/color";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import DateView from "../components/DateView";
+
+const DateScreen = ({navigation}) => {
+  const BackHandler= () =>{
+    navigation.navigate("Home");
+  }
+  function renderHeader() {
+    return (
+      <View
+        style={{  
+          width: "100%",
+          height: 100,
+          ...styles.shadow,
+          backgroundColor: "#029afd",
+        }}
+      >
+        <View
+          style={{flex:1, top:60, width: "100%", flexDirection: "row",paddingHorizontal:15}}
+        >
+            <View  style={{flexDirection: "row"}}>
+                <TouchableOpacity onPress={BackHandler}>
+                    <Icon name="arrow-back-ios" color={COLORS.white} size={23} />
+                </TouchableOpacity>
+                <Text style={{fontWeight:"700",color:COLORS.white,fontFamily:"Roboto"}} >Lịch học - lịch thi</Text>
+            </View>      
+            <TouchableOpacity style={{marginLeft:"auto"}}>
+                <Icon name="calendar-today" color={COLORS.white} size={23} />
+            </TouchableOpacity>
+           
+        </View>
+      </View>
+    );
+  }
+  function renderDateBar() {
+    return(
+        <View style={styles.datebar}>
+            <TouchableOpacity style={{borderWidth:1,width:"10%",justifyContent:"center",alignItems:"center",borderRadius:50,borderColor:COLORS.grey,marginRight:"auto"}}>
+                <Icon style={{padding:5,left:5}} name="arrow-back-ios" color={COLORS.blue} size={23} />
+            </TouchableOpacity>
+            <Text style={{color:"#029afd",textAlign:"center",fontFamily:"Roboto",fontWeight:"bold"}}>20/02/2023 - 26/02/2023</Text>
+            <TouchableOpacity style={{borderWidth:1,width:"10%",justifyContent:"center",alignItems:"center",borderRadius:50,borderColor:COLORS.grey,transform: [{rotate: '180deg'}],marginLeft:"auto"}}>
+                <Icon style={{padding:5,left:5}} name="arrow-back-ios" color={COLORS.blue} size={23} />
+            </TouchableOpacity>
+        </View>
+    );
+  }
+  function Date(){
+    return(
+        <View style={{width:"100%"}}>
+            <View style={{width:"100%",height:50,backgroundColor:COLORS.dateheaderbar,justifyContent:"center",alignItems:"center"}}>
+                <Text style={{color:"#029afd",textAlign:"center",fontFamily:"Roboto",fontWeight:"bold",fontSize:15}} >Thứ 2 - 20/02/2023</Text>
+            </View>
+            <View style={{width:"100%",height:40,backgroundColor:COLORS.white,flexDirection:"row",alignItems:"center",borderBottomWidth:0.5}}>
+                <View style={{width:5,height:"100%",backgroundColor:"#196dc7"}} ></View>
+                <Text style={{marginLeft:10,fontWeight:"bold",fontFamily:"Roboto"}}>Tiết 1 - 3</Text>
+            </View>
+            <View style={{width:"100%",minHeight:120,backgroundColor:COLORS.white,top:0.5,flexDirection:"row"}}>
+                <View style={{height:"100%",width:"10%",alignItems:"center"}}>
+                    <View style={{height:20,width:20,backgroundColor:COLORS.lichhoc,marginTop:10,borderWidth:0.8}}></View>
+                </View>
+                <View style={{height:"100%",width:"90%",padding:10}}>
+                    <Text style={{fontFamily:"Roboto",fontWeight:"bold",fontSize:17,paddingBottom:20}} >DHKTPM15B:Lập trình WWW(Lý thuyết)</Text>
+                    <Text style={{fontFamily:"Roboto",paddingBottom:10}} >Phòng:A3:02</Text>
+                    <Text style={{fontFamily:"Roboto",paddingBottom:10}} >Giảng viên:Nguyễn Văn A </Text>
+                </View>
+            </View>
+        </View>
+    );
+  }
+  return (
+    <View style={styles.container}>
+      {renderHeader()}
+      {renderDateBar()}
+      <DateView/>
+      <Text>DateScreen</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 3,
+    backgroundColor:"#f5f5f5"
+  },
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+    },
+  },
+  containerheader: {
+    flexDirection: "row",
+    top: 90,
+    with: "80%",
+    height: "40%",
+    backgroundColor: COLORS.white,
+    marginVertical: 10,
+    marginHorizontal: 10,
+    borderRadius: 15,
+  },
+  datebar:{
+    backgroundColor:COLORS.white,
+    height:"8%",
+    alignItems:"center",
+    padding:10,
+    flexDirection:"row",
+    justifyContent:"center"
+  }
+});
+
+export default DateScreen;
